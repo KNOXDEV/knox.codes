e3708513c8f060e9202bc4bf6ba0b41bac428520
KNOXDEV <nick@knox.codes>
2022-12-29 19:16:29 -0800

    no top-level wait when deploying

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,14 +1,5 @@
 import parseISO from 'date-fns/parseISO';
-import type { SvelteComponent } from 'svelte';
-
-const modules = await import.meta.glob(`$lib/posts/*.svx`);
-
-// modules is a map of full filesystem paths to resolvers,
-// but we want to be able to quickly index in with just the
-// slug that the user provides as a path parameter
-const postsBySlug = Object.fromEntries(
-	Object.entries(modules).map(([path, resolver]) => [getSlugFromPath(path), resolver])
-);
+import type {SvelteComponent} from 'svelte';
 
 export function getSlugFromPath(path: string): string {
 	const basename = path.split(/[\\/]/).pop() ?? path;
@@ -16,9 +7,14 @@ export function getSlugFromPath(path: string): string {
 }
 
 export async function getPostBySlug(slug: string): Promise<Post | undefined> {
+	const modules = await import.meta.glob(`$lib/posts/*.svx`);
+	const postsBySlug = Object.fromEntries(
+		Object.entries(modules).map(([path, resolver]) => [getSlugFromPath(path), resolver])
+	);
+
 	if (!postsBySlug[slug]) return;
 
-	const { default: component, metadata } = (await postsBySlug[slug]()) as {
+	const {default: component, metadata} = (await postsBySlug[slug]()) as {
 		default: SvelteComponent;
 		metadata: { [key: string]: string };
 	};
