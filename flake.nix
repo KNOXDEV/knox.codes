db79f2af18d0d76ecdb0fd60aafa1b334886139c
Nick Knox <nick@knox.codes>
2025-12-06 14:02:48 -0500

    feat: nix flake devshell

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.x86_64-linux.default =
+      pkgs.mkShell {
+        buildInputs = with pkgs; [ nodejs pnpm ];
+      };
+  };
+}
